c71ce537a2fda30f47ef217b7f5037eedf3805be
gnsdp <rladpgns99@gmail.com>
2025-10-03 01:32:54 +0900

    feat: API 에러 코드 enum 생성

---
+++ src/main/java/ktb3/full/week04/dto/response/ApiErrorCode.java
@@ -0,0 +1,40 @@
+package ktb3.full.week04.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ApiErrorCode {
+
+    // 400 Bad Request
+    INVALID_INPUT("4001"),
+
+    // 401 Unauthorized
+    INVALID_CREDENTIALS("4011"),
+    LOGIN_REQUIRED("4012"),
+
+    // 403 Forbidden
+    NO_PERMISSION("4031"),
+
+    // 404 Not Found
+    USER_NOT_FOUND("4041"),
+    POST_NOT_FOUND("4042"),
+    COMMENT_NOT_FOUND("4043"),
+
+    // 405 Method Not Allowed
+    METHOD_NOT_ALLOWED("4051"),
+
+    // 409 Conflict
+    DUPLICATED_EMAIL("4091"),
+    DUPLICATED_NICKNAME("4092"),
+
+    // 415 Unsupported Media Type
+    UNSUPPORTED_MEDIA_TYPE("4151"),
+
+    // 500 Internal Server Error
+    INTERNAL_SERVER_ERROR("5001"),
+    ;
+
+    private final String code;
+}
