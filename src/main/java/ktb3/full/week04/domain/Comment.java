78de48574b99fbcbe35f7c55cd082eb03353470c
gnsdp <rladpgns99@gmail.com>
2025-10-03 14:57:05 +0900

    feat: Comment 엔티티 생성

---
+++ src/main/java/ktb3/full/week04/domain/Comment.java
@@ -0,0 +1,28 @@
+package ktb3.full.week04.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Comment extends Auditing {
+
+    private Long commentId;
+    private final User user;
+    private final Post post;
+    private String content;
+    private boolean isDeleted;
+
+    public static Comment create(User user, Post post, String content) {
+        return Comment.builder()
+                .commentId(null)
+                .user(user)
+                .post(post)
+                .content(content)
+                .isDeleted(false)
+                .build();
+    }
+}
