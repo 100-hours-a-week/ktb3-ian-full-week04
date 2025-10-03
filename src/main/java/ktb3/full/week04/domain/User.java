be4748f95ba71f8d09c84f083745b1afd5e31bcd
gnsdp <rladpgns99@gmail.com>
2025-10-03 13:58:47 +0900

    feat: User 엔티티 생성

---
+++ src/main/java/ktb3/full/week04/domain/User.java
@@ -0,0 +1,30 @@
+package ktb3.full.week04.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User extends Auditing {
+
+    private Long userId;
+    private final String email;
+    private String password;
+    private String nickname;
+    private String profileImage;
+    private boolean isDeleted;
+
+    public static User create(String email, String password, String nickname, String profileImage) {
+        return User.builder()
+                .userId(null)
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .profileImage(profileImage)
+                .isDeleted(false)
+                .build();
+    }
+}
