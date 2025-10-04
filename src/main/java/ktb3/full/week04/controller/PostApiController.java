2bf2be1c5f9aff00a99c92e553914009caab5f72
gnsdp <rladpgns99@gmail.com>
2025-10-05 00:48:51 +0900

    feat: PostApiController 생성

---
+++ src/main/java/ktb3/full/week04/controller/PostApiController.java
@@ -0,0 +1,11 @@
+package ktb3.full.week04.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/posts")
+@RestController
+public class PostApiController {
+
+}
