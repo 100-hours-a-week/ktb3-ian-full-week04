221243f528f0f7dfddf56ae647561682e7a9cd70
gnsdp <rladpgns99@gmail.com>
2025-10-04 22:10:35 +0900

    feat: UserRepository 인터페이스 생성

---
+++ src/main/java/ktb3/full/week04/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package ktb3.full.week04.repository;
+
+import ktb3.full.week04.domain.User;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+
+    void save(User user);
+
+    Optional<User> findById(Long userId);
+
+    void update(User user);
+
+    void delete(User user);
+}
