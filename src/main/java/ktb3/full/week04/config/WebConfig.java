05623664e4dae34c16a9f2c30ed41f3d19807da0
gnsdp <rladpgns99@gmail.com>
2025-10-06 15:20:10 +0900

    feat: 인증 Argument Resolver 구현

--- src/main/java/ktb3/full/week04/config/WebConfig.java
+++ src/main/java/ktb3/full/week04/config/WebConfig.java
@@ -1,9 +1,13 @@
 package ktb3.full.week04.config;
 
+import ktb3.full.week04.annotation.resolver.AuthenticationArgumentResolver;
 import ktb3.full.week04.interceptor.LoginValidateInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginValidateInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/users/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthenticationArgumentResolver());
     }
 }
