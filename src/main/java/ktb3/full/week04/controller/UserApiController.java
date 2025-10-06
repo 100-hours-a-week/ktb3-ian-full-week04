39b19b3b50d30374224acf88ff69e7f0eab63279
gnsdp <rladpgns99@gmail.com>
2025-10-06 13:47:43 +0900

    feat: UserApiController 구현

--- src/main/java/ktb3/full/week04/controller/UserApiController.java
+++ src/main/java/ktb3/full/week04/controller/UserApiController.java
@@ -1,11 +1,50 @@
 package ktb3.full.week04.controller;
 
+import jakarta.servlet.http.HttpSession;
+import ktb3.full.week04.dto.request.UserLoginRequest;
+import ktb3.full.week04.dto.request.UserRegisterRequest;
+import ktb3.full.week04.dto.response.ApiResponse;
+import ktb3.full.week04.dto.session.LoggedInUser;
+import ktb3.full.week04.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static ktb3.full.week04.common.Constants.SESSION_ATTRIBUTE_NAME_LOGGED_IN_USER;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
 public class UserApiController {
 
+    private final UserService userService;
+
+    @GetMapping("/email-validation")
+    public ResponseEntity<ApiResponse<Boolean>> validateEmailAvailable(@RequestParam("email") String email) {
+        boolean available = userService.validateEmailAvailable(email);
+        return ResponseEntity.ok()
+                .body(ApiResponse.of(available));
+    }
+
+    @GetMapping("/nickname-validation")
+    public ResponseEntity<ApiResponse<Boolean>> validateNicknameAvailable(@RequestParam("nickname") String nickname) {
+        boolean available = userService.validateNicknameAvailable(nickname);
+        return ResponseEntity.ok()
+                .body(ApiResponse.of(available));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> signUp(@RequestBody UserRegisterRequest userRegisterRequest) {
+        userService.register(userRegisterRequest);
+        return ResponseEntity.ok()
+                .body(ApiResponse.getBaseResponse());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<Void>> login(@RequestBody UserLoginRequest userLoginRequest, HttpSession session) {
+        LoggedInUser loggedInUser = userService.login(userLoginRequest);
+        session.setAttribute(SESSION_ATTRIBUTE_NAME_LOGGED_IN_USER, loggedInUser);
+        return ResponseEntity.ok()
+                .body(ApiResponse.getBaseResponse());
+    }
 }
