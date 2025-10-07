7553853eb945012cbea4eeed756e78b5c001c7ce
gnsdp <rladpgns99@gmail.com>
2025-10-07 20:31:37 +0900

    feat: Findable 인터페이스 생성

---
+++ src/main/java/ktb3/full/week04/service/Findable.java
@@ -0,0 +1,8 @@
+package ktb3.full.week04.service;
+
+import ktb3.full.week04.common.exception.base.NotFoundException;
+
+public interface Findable<T, ID> {
+
+    T getOrThrow(ID id) throws NotFoundException;
+}
