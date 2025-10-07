97741aee1f0958d8c10aa0ad6417dbec6119c060
gnsdp <rladpgns99@gmail.com>
2025-10-08 00:43:15 +0900

    feat: CommentApiController 구현

--- src/main/java/ktb3/full/week04/presentation/controller/CommentApiController.java
+++ src/main/java/ktb3/full/week04/presentation/controller/CommentApiController.java
@@ -1,12 +1,61 @@
 package ktb3.full.week04.presentation.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import ktb3.full.week04.common.annotation.resolver.Authentication;
+import ktb3.full.week04.dto.page.PageRequest;
+import ktb3.full.week04.dto.page.PageResponse;
+import ktb3.full.week04.dto.request.CommentCreateRequest;
+import ktb3.full.week04.dto.request.CommentUpdateRequest;
+import ktb3.full.week04.dto.response.ApiResponse;
+import ktb3.full.week04.dto.response.CommentResponse;
+import ktb3.full.week04.dto.session.LoggedInUser;
+import ktb3.full.week04.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/posts/{postId}/comments")
 @RestController
 public class CommentApiController {
 
+    private final CommentService commentService;
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<ApiResponse<PageResponse<CommentResponse>>> getAllComments(
+            @Valid PageRequest pageRequest,
+            @Positive @PathVariable("postId") long postId) {
+        PageResponse<CommentResponse> response = commentService.getAllComments(postId, pageRequest);
+        return ResponseEntity.ok()
+                .body(ApiResponse.of(response));
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<ApiResponse<Void>> createComment(
+            @Authentication LoggedInUser loggedInUser,
+            @Positive @PathVariable("postId") long postId,
+            @Valid @RequestBody CommentCreateRequest request) {
+        commentService.createComment(loggedInUser.getUserId(), postId, request);
+        return ResponseEntity.ok()
+                .body(ApiResponse.getBaseResponse());
+    }
+
+    @PatchMapping("/comments/{commentId}")
+    public ResponseEntity<ApiResponse<Void>> updatePost(
+            @Authentication LoggedInUser loggedInUser,
+            @Positive @PathVariable("commentId") long commentId,
+            @Valid @RequestBody CommentUpdateRequest request) {
+        commentService.updateComment(loggedInUser.getUserId(), commentId, request);
+        return ResponseEntity.ok()
+                .body(ApiResponse.getBaseResponse());
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public ResponseEntity<ApiResponse<Void>> deletePost(
+            @Authentication LoggedInUser loggedInUser,
+            @Positive @PathVariable("commentId") long commentId) {
+        commentService.deleteComment(loggedInUser.getUserId(), commentId);
+        return ResponseEntity.ok()
+                .body(ApiResponse.getBaseResponse());
+    }
 }
