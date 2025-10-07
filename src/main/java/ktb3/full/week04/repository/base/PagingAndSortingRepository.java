727eec1ae02ddc622728d44b1248cfd21b32e52f
gnsdp <rladpgns99@gmail.com>
2025-10-08 00:13:17 +0900

    feat: PagingAndSortingRepository 인터페이스 공통 로직 추가

--- src/main/java/ktb3/full/week04/repository/base/PagingAndSortingRepository.java
+++ src/main/java/ktb3/full/week04/repository/base/PagingAndSortingRepository.java
@@ -1,9 +1,30 @@
 package ktb3.full.week04.repository.base;
 
+import ktb3.full.week04.dto.page.IdAndCreatedDate;
 import ktb3.full.week04.dto.page.PageRequest;
 import ktb3.full.week04.dto.page.PageResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public interface PagingAndSortingRepository<T> {
 
     PageResponse<T> findAll(PageRequest pageRequest);
+
+    default PageResponse<T> findAllByLatest(Map<Long, T> idToEntity, List<IdAndCreatedDate> latestEntities, PageRequest pageRequest) {
+        int pageNumber = pageRequest.getPage();
+        int pageSize = pageRequest.getSize();
+        int offset =  (pageNumber - 1) * pageSize;
+        int start = latestEntities.size() - offset - 1;
+        int end = Math.max(start - pageSize + 1, 0);
+
+        List<T> content = new ArrayList<>();
+        for (int i = start; i >= end; i--) {
+            IdAndCreatedDate pair = latestEntities.get(i);
+            content.add(idToEntity.get(pair.getId()));
+        }
+
+        return PageResponse.of(content, pageNumber, pageSize, end > 0);
+    }
 }
