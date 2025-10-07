54872f8535142f1007e2757380a54e6610eacde0
gnsdp <rladpgns99@gmail.com>
2025-10-07 20:46:58 +0900

    feat: Findable 인터페이스 적용

--- src/main/java/ktb3/full/week04/service/PostService.java
+++ src/main/java/ktb3/full/week04/service/PostService.java
@@ -1,23 +1,25 @@
 package ktb3.full.week04.service;
 
-import ktb3.full.week04.dto.page.Page;
-import ktb3.full.week04.dto.page.Pageable;
+import ktb3.full.week04.domain.Post;
+import ktb3.full.week04.dto.page.PageRequest;
+import ktb3.full.week04.dto.page.PageResponse;
 import ktb3.full.week04.dto.request.PostCreateRequest;
 import ktb3.full.week04.dto.request.PostUpdateRequest;
 import ktb3.full.week04.dto.response.PostDetailResponse;
 import ktb3.full.week04.dto.response.PostResponse;
+import ktb3.full.week04.service.base.Findable;
 
-public interface PostService {
+public interface PostService extends Findable<Post, Long> {
 
-    Page<PostResponse> getAllPosts(Pageable pageable);
+    PageResponse<PostResponse> getAllPosts(PageRequest pageRequest);
 
-    PostDetailResponse getPost(Long postId);
+    PostDetailResponse getPost(long postId);
 
-    void createPost(PostCreateRequest request);
+    void createPost(long userId, PostCreateRequest request);
 
-    void updatePost(Long userId, Long postId, PostUpdateRequest request);
+    void updatePost(long userId, long postId, PostUpdateRequest request);
 
-    void deletePost(Long userId, Long postId);
+    void deletePost(long userId, long postId);
 
-    void createOrUpdateLiked(Long userId, Long postId);
+    void createOrUpdateLiked(long userId, long postId);
 }
