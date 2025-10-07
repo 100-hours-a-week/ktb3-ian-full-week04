3a8eacd41152ce2afeb59d6d7e29d311147e3305
gnsdp <rladpgns99@gmail.com>
2025-10-07 22:17:28 +0900

    feat: PostServiceImpl 구현

--- src/main/java/ktb3/full/week04/dto/request/PostCreateRequest.java
+++ src/main/java/ktb3/full/week04/dto/request/PostCreateRequest.java
@@ -1,4 +1,19 @@
 package ktb3.full.week04.dto.request;
 
+import ktb3.full.week04.domain.Post;
+import ktb3.full.week04.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class PostCreateRequest {
+
+    private final String title;
+    private final String content;
+    private final String image;
+
+    public Post toEntity(User user) {
+        return Post.create(user, title, content, image);
+    }
 }
