0e86f862659548631219fe3641fedfa848cf664a
gnsdp <rladpgns99@gmail.com>
2025-10-11 00:48:00 +0900

    feat: 회원 정보 조회 API 응답 수정

--- src/main/java/ktb3/full/week04/dto/response/UserAccountResponse.java
+++ src/main/java/ktb3/full/week04/dto/response/UserAccountResponse.java
@@ -4,15 +4,19 @@ import ktb3.full.week04.domain.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @RequiredArgsConstructor
 public class UserAccountResponse {
 
+    private final long userId;
     private final String email;
     private final String nickname;
     private final String profileImage;
+    private final LocalDateTime createdAt;
 
     public static UserAccountResponse from(User user) {
-        return new UserAccountResponse(user.getEmail(), user.getNickname(), user.getProfileImage());
+        return new UserAccountResponse(user.getUserId(), user.getEmail(), user.getNickname(), user.getProfileImage(), user.getCreatedAt());
     }
 }
