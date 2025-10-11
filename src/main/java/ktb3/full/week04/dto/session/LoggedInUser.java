d540fbe17025e6c049c1b75cc6491f0467a90bc3
gnsdp <rladpgns99@gmail.com>
2025-10-11 13:43:24 +0900

    feat: 로그인 API 응답 수정

--- src/main/java/ktb3/full/week04/dto/session/LoggedInUser.java
+++ src/main/java/ktb3/full/week04/dto/session/LoggedInUser.java
@@ -1,17 +1,11 @@
 package ktb3.full.week04.dto.session;
 
-import ktb3.full.week04.domain.User;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
 public class LoggedInUser {
 
     private final long userId;
-
-    public static LoggedInUser from(User user) {
-        return new LoggedInUser(user.getUserId());
-    }
 }
