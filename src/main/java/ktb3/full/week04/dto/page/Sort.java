779458c862b57f3dbcc9654bfc4c01cf05adbbd1
gnsdp <rladpgns99@gmail.com>
2025-10-18 21:38:05 +0900

    feat: PageRequest 유효성 검사 추가

--- src/main/java/ktb3/full/week04/dto/page/Sort.java
+++ src/main/java/ktb3/full/week04/dto/page/Sort.java
@@ -1,11 +1,19 @@
 package ktb3.full.week04.dto.page;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
+
+import static ktb3.full.week04.common.Constants.MESSAGE_NOT_NULL_SORT_DIRECTION;
+import static ktb3.full.week04.common.Constants.MESSAGE_NOT_NULL_SORT_PROPERTY;
 
 @Value
 public class Sort {
 
+    @NotNull(message = MESSAGE_NOT_NULL_SORT_DIRECTION)
     Direction direction;
+
+    @NotBlank(message = MESSAGE_NOT_NULL_SORT_PROPERTY)
     String property;
 
     public static Sort asc(String property) {
