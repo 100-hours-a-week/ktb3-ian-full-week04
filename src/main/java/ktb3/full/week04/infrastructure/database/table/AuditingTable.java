472e60bea61d4c10d8573e8dee007ff710b0079b
gnsdp <rladpgns99@gmail.com>
2025-10-19 13:05:33 +0900

    refactor: Table, AuditingTable 추상 클래스화

--- src/main/java/ktb3/full/week04/infrastructure/database/table/AuditingTable.java
+++ src/main/java/ktb3/full/week04/infrastructure/database/table/AuditingTable.java
@@ -2,13 +2,8 @@ package ktb3.full.week04.infrastructure.database.table;
 
 import ktb3.full.week04.domain.base.Auditing;
 import ktb3.full.week04.infrastructure.database.identifier.IdentifierGenerator;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
-@Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-@Component
-public class AuditingTable<T extends Auditing, ID> extends Table<T, ID> {
+public abstract class AuditingTable<T extends Auditing, ID> extends Table<T, ID> {
 
     public AuditingTable(IdentifierGenerator<T, ID> identifierGenerator) {
         super(identifierGenerator);
