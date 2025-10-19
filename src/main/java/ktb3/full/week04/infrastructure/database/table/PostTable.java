c0b574266babf3b957686eaf278adff9f5a9c5de
gnsdp <rladpgns99@gmail.com>
2025-10-19 12:35:01 +0900

    refactor: CommentTable 생성

--- src/main/java/ktb3/full/week04/infrastructure/database/table/PostTable.java
+++ src/main/java/ktb3/full/week04/infrastructure/database/table/PostTable.java
@@ -10,15 +10,18 @@ import java.util.concurrent.atomic.AtomicLong;
 public class PostTable extends AuditingTable<Post, Long> {
 
     private final AtomicLong activePostCounter = new AtomicLong(0L);
+    private final PostCommentConnector postCommentConnector;
 
-    public PostTable(IdentifierGenerator<Post, Long> identifierGenerator) {
+    public PostTable(IdentifierGenerator<Post, Long> identifierGenerator, PostCommentConnector postCommentConnector) {
         super(identifierGenerator);
+        this.postCommentConnector = postCommentConnector;
     }
 
     @Override
     public Long insert(Post entity) {
         Long postId = super.insert(entity);
         activePostCounter.getAndIncrement();
+        postCommentConnector.createPost(postId);
         return postId;
     }
 
@@ -31,9 +34,10 @@ public class PostTable extends AuditingTable<Post, Long> {
     }
 
     @Override
-    public void delete(Long aLong) {
-        super.delete(aLong);
+    public void delete(Long id) {
+        super.delete(id);
         activePostCounter.getAndDecrement();
+        postCommentConnector.removePost(id);
     }
 
     public long getTotalActiveElements() {
