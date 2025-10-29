132946f71bc1279c5ab0067f973c946dfd5a3c97
gnsdp <rladpgns99@gmail.com>
2025-10-30 08:59:30 +0900

    feat: JPA 엔티티 생성

---
+++ src/main/java/ktb3/full/community/domain/entity/Comment.java
@@ -0,0 +1,51 @@
+package ktb3.full.community.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@ToString
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment extends AuditTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    public static Comment create(User user, Post post, String content) {
+        return Comment.builder()
+                .id(null)
+                .user(user)
+                .post(post)
+                .content(content)
+                .isDeleted(false)
+                .build();
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+        this.auditDeletedAt();
+    }
+}
