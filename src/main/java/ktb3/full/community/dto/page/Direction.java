0bc8cc6c02a2a07f96620b46d35c7dd9981c4e63
gnsdp <rladpgns99@gmail.com>
2025-11-01 14:17:16 +0900

    refactor: 기존 메모리 기반 DB 및 Repository 삭제

--- src/main/java/ktb3/full/community/dto/page/Direction.java
+++
@@ -1,7 +0,0 @@
-package ktb3.full.community.dto.page;
-
-public enum Direction {
-
-    ASC,
-    DESC;
-}
--- src/main/java/ktb3/full/community/infrastructure/database/identifier/IdentifierGenerator.java
+++
@@ -1,6 +0,0 @@
-package ktb3.full.community.infrastructure.database.identifier;
-
-public interface IdentifierGenerator<T, ID> {
-
-    ID generate(T entity);
-}
