52d8a36dbb32cadf03f574593da5bb105d9c3f3b
gnsdp <rladpgns99@gmail.com>
2025-11-01 21:44:50 +0900

    feat: 댓글 목록 조회 N+1 문제 해결

--- src/main/java/ktb3/full/community/repository/CommentRepository.java
+++ src/main/java/ktb3/full/community/repository/CommentRepository.java
@@ -4,8 +4,12 @@ import ktb3.full.community.domain.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    Page<Comment> findByPostId(long postId, Pageable pageable);
+    @Query(value = "select c from Comment c join fetch c.user where c.post.id = :postId order by c.createdAt desc",
+            countQuery = "select count(p) from Comment p where p.post.id = :postId")
+        Page<Comment> findAllLatestByPostId(@Param("postId") long postId, Pageable pageable);
 }
