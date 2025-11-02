715f0bc01dcdeabdc24b9d3d80a2718dfc26ea63
gnsdp <rladpgns99@gmail.com>
2025-11-02 15:40:40 +0900

    feat: 회원 조회 시 탈퇴 여부 검사

--- src/main/java/ktb3/full/community/repository/UserRepository.java
+++ src/main/java/ktb3/full/community/repository/UserRepository.java
@@ -2,6 +2,8 @@ package ktb3.full.community.repository;
 
 import ktb3.full.community.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByNickname(String nickname);
 
-    Optional<User> findByEmail(String email);
+    @Query("select u from User u where u.email = :email and u.isDeleted = false")
+    Optional<User> findByEmail(@Param("email") String email);
 }
