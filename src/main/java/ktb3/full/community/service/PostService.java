378505a18441eca50137b469872b6e5472f7989a
gnsdp <rladpgns99@gmail.com>
2025-11-11 15:33:05 +0900

    feat: 게시글 이미지 업로드 기능 구현

--- src/main/java/ktb3/full/community/service/PostService.java
+++ src/main/java/ktb3/full/community/service/PostService.java
@@ -9,11 +9,19 @@ import ktb3.full.community.dto.response.PostDetailResponse;
 import ktb3.full.community.dto.response.PostResponse;
 import ktb3.full.community.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -23,6 +31,12 @@ public class PostService {
     private final PostRepository postRepository;
     private final UserService userService;
     private final PostLikeService postLikeService;
+
+    @Value("${file.path.base}")
+    private String fileBasePath;
+
+    @Value("${file.path.image}")
+    private String fileImagePath;
 
     public PagedModel<PostResponse> getAllPosts(Pageable pageable) {
         Page<Post> postPages = postRepository.findAll(pageable);
@@ -38,9 +52,11 @@ public class PostService {
     }
 
     @Transactional
-    public PostDetailResponse createPost(long userId, PostCreateRequest request) {
+    public PostDetailResponse createPost(long userId, PostCreateRequest request) throws IOException {
+        MultipartFile image = request.getImage();
+        String imagePath = saveImageAndGetPath(request.getImage());
         User user = userService.getOrThrow(userId);
-        Post post = request.toEntity(user);
+        Post post = request.toEntity(user, imagePath, image.getOriginalFilename());
 
         postRepository.save(post);
 
@@ -48,7 +64,7 @@ public class PostService {
     }
 
     @Transactional
-    public PostDetailResponse updatePost(long userId, long postId, PostUpdateRequest request) {
+    public PostDetailResponse updatePost(long userId, long postId, PostUpdateRequest request) throws IOException {
         Post post = getOrThrow(postId);
         userService.validatePermission(userId, post.getUser().getId());
 
@@ -61,7 +77,8 @@ public class PostService {
         }
 
         if (request.getImage() != null) {
-            post.updateImage(request.getImage());
+            String imagePath = saveImageAndGetPath(request.getImage());
+            post.updateImage(imagePath);
         }
 
         boolean liked = postLikeService.isLiked(userId, postId);
@@ -82,5 +99,18 @@ public class PostService {
     public Post getForUpdateOrThrow(long postId) {
         return postRepository.findByIdForUpdate(postId)
                 .orElseThrow(PostNotFoundException::new);
+    }
+
+    private String saveImageAndGetPath(MultipartFile image) throws IOException {
+        String imagePath = null;
+
+        if (image != null) {
+            imagePath = this.fileImagePath + "/" + UUID.randomUUID() + image.getOriginalFilename();
+            Path path = Paths.get(this.fileBasePath + imagePath);
+            Files.createDirectories(path.getParent());
+            Files.write(path, image.getBytes());
+        }
+
+        return imagePath;
     }
 }
